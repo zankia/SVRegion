baaba82be4de41b9117670a4e45e385f958c07f8
zankia <zankia16@gmail.com>
2017-02-04 15:14:18 +0100

    added remaining days decrementation

--- src/fr/zankia/svregion/Scheduler.java
+++ src/fr/zankia/svregion/Scheduler.java
@@ -1,11 +1,23 @@
 package fr.zankia.svregion;
 
+import org.bukkit.configuration.ConfigurationSection;
+
 public class Scheduler implements Runnable {
 
 	@Override
 	public void run() {
 		//reduce all region days in config
 		//delete the region if days < 0
+		ConfigurationSection regions = SVR.getRegions().getConfigurationSection("regions");
+		for(String uuid : regions.getKeys(false)) {
+			int remaining = regions.getInt(uuid + ".remaining");
+			if(remaining > 0) {
+				regions.set(uuid + ".remaining", --remaining);
+			} else {
+				regions.set(uuid, null);
+			}
+		}
+		SVR.saveRegions();
 	}
 
 }
